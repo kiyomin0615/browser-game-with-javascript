1ae7ccd3776d94c3ba412a30cbeba506258a441a
Kyomin Ku <moeday1010@gmail.com>
2023-12-29 14:00:07 +0900

    add basic html, css, js files

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  const canvasEl = this.document.getElementById("canvas1");
+  const context = canvasEl.getContext("2d");
+  canvasEl.width = 1500;
+  canvasEl.height = 500;
+});
