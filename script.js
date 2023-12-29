d5981e6e92c5141a70b1d873fd9816d48fd3c3e9
Kyomin Ku <moeday1010@gmail.com>
2023-12-29 14:03:49 +0900

    add classes to js file for the game

--- script.js
+++ script.js
@@ -3,4 +3,40 @@ window.addEventListener("load", function () {
   const context = canvasEl.getContext("2d");
   canvasEl.width = 1500;
   canvasEl.height = 500;
+
+  class InputHandler {
+
+  }
+
+  class Projectile {
+
+  }
+
+  class Particle {
+
+  }
+
+  class Player {
+
+  }
+
+  class Enemy {
+
+  }
+
+  class Layer {
+
+  }
+
+  class Background {
+
+  }
+
+  class UI {
+
+  }
+
+  class Game {
+    
+  }
 });
