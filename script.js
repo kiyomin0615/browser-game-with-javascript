ea2b92270008133dd6cda49c7dca7f4555d7f3a6
Kyomin Ku <moeday1010@gmail.com>
2023-12-29 14:22:24 +0900

    create player & game objects

--- script.js
+++ script.js
@@ -4,39 +4,54 @@ window.addEventListener("load", function () {
   canvasEl.width = 1500;
   canvasEl.height = 500;
 
-  class InputHandler {
+  class InputHandler {}
 
-  }
+  class Projectile {}
 
-  class Projectile {
-
-  }
-
-  class Particle {
-
-  }
+  class Particle {}
 
   class Player {
+    constructor(game) {
+      this.game = game;
+      this.width = 120;
+      this.height = 190;
+      this.x = 20;
+      this.y = 100;
+    }
 
+    update() {
+      this.y += this.speedY;
+    }
+
+    draw(context) {
+      context.fillRet(this.x, this.y, this.width, this.height);
+    }
   }
 
-  class Enemy {
+  class Enemy {}
 
-  }
+  class Layer {}
 
-  class Layer {
+  class Background {}
 
-  }
-
-  class Background {
-
-  }
-
-  class UI {
-
-  }
+  class UI {}
 
   class Game {
-    
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.player = new Player(this);
+    }
+
+    update() {
+      this.player.update();
+    }
+
+    draw(context) {
+      this.player.draw(context);
+    }
   }
+
+  const game = new Game(canvasEl.width, canvasEl.height);
+  
 });
